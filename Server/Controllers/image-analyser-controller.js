42a63806ecf87a996b72b56198dc6b52f4e71bd0
tomnotthomas <tom.schwabe1@outlook.de>
2024-01-12 17:22:08 +0100

    add total invoice amount to schema

--- Server/Controllers/image-analyser-controller.js
+++ Server/Controllers/image-analyser-controller.js
@@ -1,4 +1,4 @@
-import { TextractClient, AnalyzeDocumentCommand } from "@aws-sdk/client-textract";
+import { TextractClient, AnalyzeExpenseCommand, AnalyzeDocumentCommand } from "@aws-sdk/client-textract";
 import { fromIni } from '@aws-sdk/credential-providers';
 import 'dotenv/config'
 import RawTextOutput from '../Models/raw-ocr-output-model.js'
@@ -25,33 +25,55 @@ const params = {
   FeatureTypes: ['TABLES', 'FORMS'],
 };
 
+  //if it is an invoice, we can get the total amount to be paid.
+  const command = new AnalyzeExpenseCommand(params);
+  const totalResponse = await textractClient.send(command);
+  const positionInArray  = totalResponse.ExpenseDocuments[0]
+  console.log({positionInArray});
+
 const displayBlockInfo = async (response) => {
   try {
     let words = [];
     let handwriting= false;
-
     response.Blocks.forEach(block => {
       // Filter for lines and words only
       if ( block.BlockType === 'WORD') {
         words.push(block.Text.toLowerCase());
       }
+    // Check whether we have handwriting in the document. We can use that to recognize the POD
      if (block.TextType === 'HANDWRITING'){
       handwriting = true;
      }
     })
-    console.log(words);
- 
 
-    const rawOutput = await RawTextOutput.create({
-      filename: req.body.photo,
-      text: JSON.stringify(words),
-      //If the document includes an Iban and an Account owner, it is an Invoice
-      invoice: (words.includes('kontoinhaber:') || words.includes('kontoinhaber') && words.includes('iban') || words.includes('iban:')),
-      pod: handwriting ,
-      //I will take the name of the document for now.
-      order: (words.includes('bestellung') || words.includes('bestellung:') || words.includes('transportauftrag') || words.includes('transportauftrag:') || words.includes ('transportauftrag, ') || words.includes('transportauftrag,'))
-    })
-    res.status(201).send(rawOutput);
+//Find the total price (total amount for the invoice)
+function findPriceText(data) {
+  for (const item of data) {
+      if (item.Type && item.Type.Text === "PRICE") {
+          return item.ValueDetection ? item.ValueDetection.Text : null;
+      }
+  }
+  return null; // Return null if no matching price type is found
+}
+
+
+
+const priceText = findPriceText(totalResponse.ExpenseDocuments[0].LineItemGroups[0].LineItems[0].LineItemExpenseFields);
+console.log(priceText);
+
+
+
+
+const rawOutput = await RawTextOutput.create({
+  filename: req.body.photo,
+  text: JSON.stringify(words),
+  //If the document includes an Iban and an Account owner, it is an Invoice
+  invoice: JSON.stringify({isinvoice: (words.includes('kontoinhaber:') || words.includes('kontoinhaber') && words.includes('iban') || words.includes('iban:') || req.body.photo.split(0,8) === 'Rechnung' ), price: priceText}),
+  pod: handwriting ,
+  //I will take the name of the document for now.
+  order: (words.includes('bestellung') || words.includes('bestellung:') || words.includes('transportauftrag') || words.includes('transportauftrag:') || words.includes ('transportauftrag, ') || words.includes('transportauftrag,') || req.body.photo.split(0,7)=== 'Auftrag')
+}) 
+res.status(201).send(rawOutput);
   } catch (err) {
     console.log("Error", err);
   }
