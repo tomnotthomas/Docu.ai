f7df75ec967aa144dc2f2482e49f367f4de2e951
Giovanni Silvotti <giopr.silvotti@yahoo.com>
2024-01-20 17:02:37 +0100

    type s3helper start

--- Server/src/Helpers/s3-find-files.ts
+++ Server/src/Helpers/s3-find-files.ts
@@ -1,22 +1,27 @@
 import {
   S3Client,
+  S3ClientConfig,
   // This command supersedes the ListObjectsCommand and is the recommended way to list objects.
   ListObjectsV2Command,
-  PutObjectCommand
+  
 } from "@aws-sdk/client-s3";
 import 'dotenv/config'
 import { fromIni } from '@aws-sdk/credential-providers';
 
-
 //REGION is the region of your AWS account, credentials profile is your profile name
-const client = new S3Client({
-  region: process.env.REGION,
-  credentials: fromIni({ profile: process.env.PROFILE }),
-});
+const credentials : { profile : string} ={ profile : process.env.PROFILE || 'default'};
+const region : string = process.env.REGION || 'default'
+
+const s3Config : S3ClientConfig = {
+  region,
+  credentials: fromIni(credentials),
+}
+
+const client  : S3Client = new S3Client(s3Config);
 
 
 //Get all the documents from the specified S3 bucket.
-const findFiles = async function () {
+const findFiles : () => Promise<string[] | undefined> = async function () {
   const command = new ListObjectsV2Command({
     Bucket: process.env.MY_BUCKET,
     // The default and maximum number of keys returned is 1000. This limits it to
