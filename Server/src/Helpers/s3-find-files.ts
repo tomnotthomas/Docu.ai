732cd4e36c15a4ff2ca79adcf6c099dbaeca1032
Giovanni Silvotti <giopr.silvotti@yahoo.com>
2024-01-20 18:50:50 +0100

    feat : Typescript s3-find-files & get-all-docs

--- Server/src/Helpers/s3-find-files.ts
+++ Server/src/Helpers/s3-find-files.ts
@@ -3,7 +3,7 @@ import {
   S3ClientConfig,
   // This command supersedes the ListObjectsCommand and is the recommended way to list objects.
   ListObjectsV2Command,
-  
+  _Object,  
 } from "@aws-sdk/client-s3";
 import 'dotenv/config'
 import { fromIni } from '@aws-sdk/credential-providers';
@@ -21,8 +21,8 @@ const client  : S3Client = new S3Client(s3Config);
 
 
 //Get all the documents from the specified S3 bucket.
-const findFiles : () => Promise<string[] | undefined> = async function () {
-  const command = new ListObjectsV2Command({
+const findFiles = async function () : Promise<(string | undefined)[] | undefined> {
+  const command : ListObjectsV2Command = new ListObjectsV2Command({
     Bucket: process.env.MY_BUCKET,
     // The default and maximum number of keys returned is 1000. This limits it to
     // one for demonstration purposes.
@@ -30,17 +30,26 @@ const findFiles : () => Promise<string[] | undefined> = async function () {
   });
 
   try {
-    let isTruncated = true;
+    let isTruncated : boolean | undefined = true;
 
-    let contents = [];
+    let contents : (string| undefined)[] = [];
+
+     interface ListObjectsResponseofInterest { 
+      Contents?: _Object[]; 
+      IsTruncated? : boolean;
+      NextContinuationToken? : string; 
+    } 
 
     while (isTruncated) {
-      const { Contents, IsTruncated, NextContinuationToken } =
-        await client.send(command);
-      const contentsList = Contents.map((c) => `${c.Key}`).join(",");
+      const { Contents, IsTruncated, NextContinuationToken } : ListObjectsResponseofInterest
+      = await client.send(command);
+        console.log("here is Contents:", Contents);
+      const contentsList: string | undefined = Contents?.map((c) => `${c.Key}`).join(",");
       contents.push (contentsList);
       isTruncated = IsTruncated;
-      command.input.ContinuationToken = NextContinuationToken;
+      if (NextContinuationToken) {
+        command.input.ContinuationToken = NextContinuationToken;
+      }
     }
     return contents;
     
